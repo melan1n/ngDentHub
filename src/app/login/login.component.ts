8ebe6058266387f6c38c94f1be91ca91e15a8bd3
Milen Nikolov <milennikolov_bg@yahoo.com>
2020-02-22 03:23:49 +0200

    login form validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user/user.service';
-import { FormGroup, Validators, FormBuilder, FormControl } from '@angular/forms';
+import { FormGroup, FormsModule, Validators, FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -12,6 +12,7 @@ export class LoginComponent implements OnInit {
   public loginForm: FormGroup; 
   errorMessage: string;
   successMessage: string;
+  emailRegex= /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g;
   
   constructor(
     private userService: UserService
