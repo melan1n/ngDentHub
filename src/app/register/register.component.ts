1fb16c5163ea277e362723c861f8a2183400d340
Milen Nikolov <milennikolov_bg@yahoo.com>
2020-02-22 09:52:22 +0200

    register form validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -12,6 +12,7 @@ export class RegisterComponent implements OnInit {
   public registerForm: FormGroup; 
   errorMessage: string;
   successMessage: string;
+  emailRegex= /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g;
 
   constructor(private userService: UserService) { 
       this.registerForm = new FormGroup({
@@ -19,7 +20,6 @@ export class RegisterComponent implements OnInit {
         password: new FormControl()
      });
     }
-
     
   ngOnInit(): void {
   }
